17e74073d88340bf69552faa16a44957a4abe0db
James Peters <email@jamesdpeters.com>
2020-11-05 16:04:14 +0000

    Chapter 2 - Exercise 11
    Using namespaces

--- Chapter 2/Ex2_10/Ex2_10/Ex2_10.cpp
+++ Chapter 2/Ex2_10/Ex2_10/Ex2_10.cpp
@@ -1,4 +1,4 @@
-// Ex2_10.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Ex2_11.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // Namespaces
 
 #include <iostream>
@@ -8,9 +8,11 @@ namespace test_namespace
 	int value{};
 }
 
+using namespace test_namespace;
+
 int main()
 {
     std::cout << "Enter an integer\n";
-	std::cin >> test_namespace::value;
-	std::cout << "\nYou entered " << test_namespace::value;
+	std::cin >> value;
+	std::cout << "\nYou entered " << value;
 }
