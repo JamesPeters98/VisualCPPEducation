23b71658cd231c5f00c9469bc86ec1ec0a9a9d3f
James Peters <email@jamesdpeters.com>
2020-11-12 13:01:01 +0000

    Chapter7-Question2
    Updated class for Question 2.

--- Chapter 7/Chapter7-Question1/Chapter7-Question1/Chapter7-Question1.cpp
+++ Chapter 7/Chapter7-Question1/Chapter7-Question1/Chapter7-Question1.cpp
@@ -7,19 +7,43 @@ struct Sample
 {
 	int data1;
 	int data2;
+	char* sPtr = nullptr;
 };
 
 int main()
 {
-	Sample a{ 10, 42 };
+	char* p_str = new char[] {"Hello world!"};
+	
+	Sample a{ 10, 42, p_str };
 	Sample b{};
 
 	std::cout << "init values in a: " << a.data1 << " & " << a.data2 << std::endl;
 	std::cout << "init values in b: " << b.data1 << " & " << b.data2 << std::endl;
+	std::cout << "init string in a: " << a.sPtr << std::endl;
+	std::cout << std::endl;
 
 	b = a;
 
 	std::cout << "After assingment of b = a" << std::endl;
 	std::cout << "values in a: " << a.data1 << " & " << a.data2 << std::endl;
 	std::cout << "values in b: " << b.data1 << " & " << b.data2 << std::endl;
+	std::cout << "string in a: " << a.sPtr << " & b: " << b.sPtr << std::endl;
+	std::cout << std::endl;
+
+	char string[] { " This is a new string!" };
+	strncat_s(a.sPtr, strlen(a.sPtr) + strlen(string) + 2, string, strlen(string));
+
+	std::cout << "After changing a.p_str" << std::endl;
+	std::cout << "values in a: " << a.data1 << " & " << a.data2 << std::endl;
+	std::cout << "values in b: " << b.data1 << " & " << b.data2 << std::endl;
+	std::cout << "string in a: " << a.sPtr << " & b: " << b.sPtr << std::endl;
+	std::cout << std::endl;
+
+	// This changes the string for both a AND b, since they both point to the same char array.
+	// To get around this the pointer value should be changed to point to a new string.
+
+	b.sPtr = new char[] {"B now has a different string!"};
+
+	std::cout << "After pointing new string to b.p_str" << std::endl;
+	std::cout << "string in a: " << a.sPtr << " & b: " << b.sPtr << std::endl;
 }
